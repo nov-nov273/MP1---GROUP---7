8ec7efe3273f094b1878b5d79cce7128516e451e
kaccthony <113964745+kaccanthony@users.noreply.github.com>
2026-04-24 07:02:24 +0800

    Updated condition

--- ex15.py
+++ ex15.py
@@ -1,6 +1,9 @@
 # 15
-
-feet = float(input('Enter your number in feet: '))
+feet = 0
+while feet <= 0:
+    feet = float(input('Enter your number in feet: '))
+    if feet <= 0:
+        print("Please enter a positive number for the height in feet.")
 
 inch = float(feet) * 12
 miles = float(feet) / 5280
