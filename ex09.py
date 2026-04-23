c9cc39d792da0d08d0037371191b8abbb33e6c61
kaccthony <113964745+kaccanthony@users.noreply.github.com>
2026-04-24 06:36:57 +0800

    minor update sa formula, function nalang sha

--- ex09.py
+++ ex09.py
@@ -7,9 +7,9 @@ while principal <= 0:
         print("Please enter a positive number for the amount deposited.")
 
 rate = 0.04
-year1 = principal * (1 + rate) ** 1
-year2 = principal * (1 + rate) ** 2
-year3 = principal * (1 + rate) ** 3
+year1 = principal * pow(1 + rate, 1)
+year2 = principal * pow(1 + rate, 2)
+year3 = principal * pow(1 + rate, 3)
 
 print(f"After 1 year: {year1:.2f}")
 print(f"After 2 years: {year2:.2f}")
